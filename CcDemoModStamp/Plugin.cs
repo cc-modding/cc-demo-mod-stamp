bdff50e378ee08aef2ca5c902306db4ff91bedac
sbcomputertech <sbcomputertech.0@gmail.com>
2023-01-12 21:36:53 +0000

    Initial Commit

---
+++ CcDemoModStamp/Plugin.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using BepInEx;
+using BepInEx.Unity.Bootstrap;
+using TMPro;
+using UnityEngine;
+
+namespace CcDemoModStamp
+{
+    [BepInPlugin(PluginInfo.PluginGuid, PluginInfo.PluginName, PluginInfo.PluginVersion)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private bool hasSetText;
+        private void Awake()
+        {
+            Logger.LogInfo($"Plugin {PluginInfo.PluginGuid} is loaded!");
+        }
+
+        private void Update()
+        {
+            if (!hasSetText)
+            {
+                try
+                {
+                    var text = GameObject.Find("Version").GetComponent<TextMeshProUGUI>();
+                    text.text += "\nModding by @reddust9#4805\n\nMods loaded:\n";
+                    foreach (var plugins in UnityChainloader.Instance.Plugins.Values)
+                    {
+                        var plName = plugins.Metadata.Name;
+                        var plVer = plugins.Metadata.Version;
+                        text.text += $"- {plName} v{plVer}\n";
+                    }
+                    
+                    hasSetText = true;
+                } catch
+                {
+                    //Logger.LogInfo("Failed to set text. Retrying...");
+                    // error means nothing, it works anyway
+                }
+            }
+        }
+    }
+}
---
+++ CcDemoModStamp/PluginInfo.cs
@@ -0,0 +1,8 @@
+﻿namespace CcDemoModStamp;
+
+public static class PluginInfo
+{
+    public const string PluginName = "CC Demo Mod Stamp";
+    public const string PluginGuid = "com.reddust9.cc.demomodstamp";
+    public const string PluginVersion = "1.0.0";
+}
